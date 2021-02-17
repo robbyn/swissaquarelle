e187fef4eed5c55b054086ca6f805e6804b9293c
Maurice Perry <maurice@perry.productions>
2021-02-17 02:52:18 +0100

    Extracted class ServletInitializer

--- src/main/java/org/tastefuljava/swissaquarelle/Application.java
+++ src/main/java/org/tastefuljava/swissaquarelle/Application.java
@@ -2,20 +2,11 @@ package org.tastefuljava.swissaquarelle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("file:conf/swissaquarelle.properties")
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(
-            SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
-    }
-
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ src/main/java/org/tastefuljava/swissaquarelle/ServletInitializer.java
@@ -0,0 +1,16 @@
+package org.tastefuljava.swissaquarelle;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("file:conf/swissaquarelle.properties")
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
